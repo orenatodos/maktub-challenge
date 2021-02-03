8e9217bf425efbc4abf11867bf6446801e7f6cef
Renato Silva <orenatodos@gmail.com>
2021-02-03 00:25:55 -0300

    :sparkles: feat: realiza cadastro de herois

--- backend/src/routes/heroes.routes.ts
+++ backend/src/routes/heroes.routes.ts
@@ -1,9 +1,28 @@
 import { Router } from 'express';
+import { v4 as uuid } from 'uuid';
 
 const heroesRouter = Router();
 
+const heroes = [];
+
 heroesRouter.post('/', (request, response) => {
-  return response.json({ message: 'Hello world' });
+  const { name, description, image } = request.body;
+
+  const { short, full } = description;
+
+  const hero = {
+    id: uuid(),
+    name,
+    description: {
+      short,
+      full,
+    },
+    image,
+  };
+
+  heroes.push(hero);
+
+  return response.json(hero);
 });
 
 export default heroesRouter;
