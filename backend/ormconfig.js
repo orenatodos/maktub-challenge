4838602cc83b7b7a432623461ba9a0b23ca3a1e4
Renato Silva <orenatodos@gmail.com>
2021-02-03 14:39:13 -0300

    :sparkles: feat: cria tabela de herois no banco de dados

--- backend/ormconfig.js
+++ backend/ormconfig.js
@@ -5,4 +5,8 @@ module.exports = {
   username: process.env.DB_USERNAME,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
+  migrations: ['./src/database/migrations/*.ts'],
+  cli: {
+    migrationsDir: './src/database/migrations',
+  },
 };
---
+++ backend/src/database/migrations/1612368229824-CreateHeroes.ts
@@ -0,0 +1,49 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export default class CreateHeroes1612368229824 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'heroes',
+        columns: [
+          {
+            name: 'id',
+            type: 'int',
+            isPrimary: true,
+            generationStrategy: 'increment',
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'short_description',
+            type: 'varchar',
+          },
+          {
+            name: 'full_description',
+            type: 'varchar',
+          },
+          {
+            name: 'image',
+            type: 'varchar',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('heroes');
+  }
+}
