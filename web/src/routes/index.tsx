bff0c7e3cc7e7a5c6235d5ac65a37b008d8210c3
Renato Silva <orenatodos@gmail.com>
2021-02-04 14:17:00 -0300

    :sparkles: feat: cria rota de heroi

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
 import Heroes from '../pages/Heroes';
+import Hero from '../pages/Hero';
 
 export default function Routes() {
   return (
@@ -11,6 +12,7 @@ export default function Routes() {
         <Route path="/" exact component={SignIn} />
         <Route path="/signup" exact component={SignUp} />
         <Route path="/heroes" exact component={Heroes} />
+        <Route path="/heroes/:id" exact component={Hero} />
       </Switch>
     </Router>
   );
