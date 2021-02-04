4e5026bdd83f4d42e42b3c933445c3c3fe78d9f5
Renato Silva <orenatodos@gmail.com>
2021-02-04 00:53:18 -0300

    :sparkles: feat: habilita cors

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,7 @@
 import 'reflect-metadata';
 
 import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
 
 import 'express-async-errors';
 
@@ -12,6 +13,7 @@ import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
