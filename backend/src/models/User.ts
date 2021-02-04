cba375bdf55b5fb1e562b2bdfdd53bc52ec413e1
Renato Silva <orenatodos@gmail.com>
2021-02-03 23:40:30 -0300

    :sparkles: feat: cria model de usuario

---
+++ backend/src/models/User.ts
@@ -0,0 +1,28 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export default class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  role: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
